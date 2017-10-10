ed494752f15b1cb7162128bb5313a3cc6ed38a37
RD <richard.thericgroup@hotmail.com>
2017-10-10 21:48:08 +1100

    rewire

--- spies/app.test.js
+++ spies/app.test.js
@@ -9,11 +9,11 @@ const should = chai.should();
 describe('app', ()=>{
     it('should call the spy correctly', ()=>{
         var spy= chai.spy();
-        //spy('Jake', 25);
-        spy('foo');
+        spy('Jake', 25);
+        //spy('foo');
         spy.should.have.been.called();
         //spy.should.have.been.called.with('Jake', 25);
-        //expect(spy).to.have.been.called.with('Jake', 25);
-        expect(spy).to.have.been.called.with('foo');
+        expect(spy).to.have.been.called.with('Jake', 25);
+        //expect(spy).to.have.been.called.with('foo');
     })
 })
