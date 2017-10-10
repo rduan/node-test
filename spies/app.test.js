ad70259ae89b049091e4aebee93fc3aee5d3bb73
RD <richard.thericgroup@hotmail.com>
2017-10-10 21:24:16 +1100

    spy

--- spies/app.test.js
+++ spies/app.test.js
@@ -1,8 +1,19 @@
-const expect = require('chai').expect;
+
+const spies = require('chai-spies-next');
+var chai = require('chai');
+
+chai.use(spies);
+const expect = chai.expect;
+const should = chai.should();
 
 describe('app', ()=>{
     it('should call the spy correctly', ()=>{
-        var spy= expect.createSpy();
-        spy();
+        var spy= chai.spy();
+        //spy('Jake', 25);
+        spy('foo');
+        spy.should.have.been.called();
+        //spy.should.have.been.called.with('Jake', 25);
+        //expect(spy).to.have.been.called.with('Jake', 25);
+        expect(spy).to.have.been.called.with('foo');
     })
 })
