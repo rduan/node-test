6d5f7188f0211f0f0939a34734c9a72e9aa1d5af
RD <richard.thericgroup@hotmail.com>
2017-10-10 21:59:22 +1100

    use spy

--- spies/app.test.js
+++ spies/app.test.js
@@ -6,7 +6,19 @@ chai.use(spies);
 const expect = chai.expect;
 const should = chai.should();
 
+const rewire = require('rewire');
+
+var app = rewire('./app');
+
+
 describe('app', ()=>{
+
+    var db = {
+        saveUser: chai.spy()
+    };
+
+    app.__set__('db', db);
+
     it('should call the spy correctly', ()=>{
         var spy= chai.spy();
         spy('Jake', 25);
@@ -15,5 +27,14 @@ describe('app', ()=>{
         //spy.should.have.been.called.with('Jake', 25);
         expect(spy).to.have.been.called.with('Jake', 25);
         //expect(spy).to.have.been.called.with('foo');
-    })
+    });
+
+    it('should call saveUser with user object', ()=>{
+        var email = 'jack@email.com';
+        var password ='pass';
+
+        app.handleSignup(email,password);
+
+        expect(db.saveUser).to.have.been.called.with({email,password});
+    });
 })
